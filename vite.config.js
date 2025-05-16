323f00bf0d4779de92f97865e5ecdc4fe56aa8fb
mushkan-kumari <mushkank1307@gmail.com>
2025-05-16 10:21:11 +0530

    first commit

--- vite.config.js
+++ vite.config.js
@@ -5,5 +5,5 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(),tailwindcss()],
-  base: "/Todo_List_SKS_T3",
+  base: "/Todo_List_SKS_T3/",
 })
